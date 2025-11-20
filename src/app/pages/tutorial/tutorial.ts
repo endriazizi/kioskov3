39065bdebb24247a8ad8ed01e8bfa42c811f1119
endriazizi <endri.azizi@gmail.com>
2025-11-21 00:58:21 +0100

    tavoli start

--- src/app/pages/tutorial/tutorial.ts
+++ src/app/pages/tutorial/tutorial.ts
@@ -84,7 +84,7 @@ export class TutorialPage implements OnInit, AfterViewInit, OnDestroy {
 
   ads: AdItem[] = [
     { kind: "image", src: "assets/poster/a3_11.jpg" },
-    { kind: "image", src: "assets/poster/a3_12.jpg" },
+    { kind: "image", src: "assets/poster/a3_10.jpg" },
     { kind: "image", src: "assets/poster/a3_13.jpg" },
     { kind: "image", src: "assets/poster/a3_14.jpg" }, // poster A3 verticale 720x1280
     { kind: "video", src: "assets/poster/eclissi.mp4" }, // video carosello
